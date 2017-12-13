d2340778edc52d11bdd058317a6ab42f4318ee61
avanegasp <arco527@gmail.com>
2017-12-13 15:09:23 -0500

    all question, begin answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+   before_action :authenticate_user! , only: [:new]
 
   def index
     @questions = Question.all
@@ -8,4 +9,23 @@ class QuestionsController < ApplicationController
   	@question = Question.new
   end
 
+  def create
+    @question = current_user.questions.create(valid_params)
+    if @question.save
+      redirect_to question_path(@question.id)
+    else
+      puts current_user
+      render :new
+  end
+end
+
+  def show
+    @question = Question.find(params[:id])
+    @answer = Answer.new
+  end
+
+  private
+    def valid_params()
+    params.require(:question).permit(:title, :description)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# angie = User.create(name:"angie", last_name:"vanegas", country:"Colombia", age:31, email:"a@a.com", password:"1234567")
+angie = User.create(name:"angie", last_name:"vanegas", country:"Colombia", age:31, email:"angie@a.com", password:"1234567")
+
+q1 = angie.questions.create(title:"Mi primera pregunta", description:"Acá esta mi primera pregunta")
+
+cq1 = q1.comments.create(description:"comentario de la pregunta 1", user_id: angie.id)
+
+aq1 = Question.find(1).answers.create(description:"respuesta", user_id: User.find(1).id)
+
+v1 = Question.find(1).votes.create(number: 3, user_id: User.find(1).id)
