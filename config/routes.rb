f1fc8518f4b4fabfc7c5b4d61cdad42f385004b0
OEM Configuration (temporary user) <davidndy@gmail.com>
2017-12-14 14:12:59 -0500

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@
 Rails.application.routes.draw do
   root "questions#index"
   devise_for :users
-  resources :questions
+  concern :categorizable do
+    resources :comments
+  end
+  resources :questions, concerns: :categorizable
+  resources :answers, concerns: :categorizable
+
+#   resources :questions do
+#   	resources :comments, shallow: true
+# end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
