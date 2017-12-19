6975925ee5358df4196a79862de0cad514889676
avanegasp <arco527@gmail.com>
2017-12-19 10:18:06 -0500

    Fix redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,15 @@ module ApplicationHelper
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
     md = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    syntax_highlighter(md.render(text, *options).to_html).html_safe
+    syntax_highlighter(md.render(text)).html_safe
+  end
+
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
   end
 end
 
