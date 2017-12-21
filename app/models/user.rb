86f5487e339dcb9b75731ec4b57e54394f3875ed
avanegasp <arco527@gmail.com>
2017-12-20 21:49:19 -0500

    Email must be uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_many :answers 
+   has_many :answers
    has_many :questions
    has_many :votes
-   has_many :comments 
+   has_many :comments
+
+   validates :email, uniqueness: true
 
 end
